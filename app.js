e77a7756e0864e8f6856f7e785e28836883a52f6
Karuturi <sxk210045@utdallas.edu>
2021-11-27 18:33:40 -0600

    changes

--- app.js
+++ app.js
@@ -11,31 +11,13 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-//const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-//const User = require('./models/user');
+const ticketRoutes = require('./routes/tickets');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-/*app.use((req, res, next) => {
-  User.findById('5d36fe2016a4e125f8826f84')//5d491ddbdca5b60274c2aa8f 5d30b7e9c62d0c324846115e
-    .then(user => {
-	  console.log(user);
-      req.user = new User(user.name,user.email,user.cart,user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});*/
-
-//app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+app.use(ticketRoutes);
 
 app.use(errorController.get404);
 
 app.listen(3000);
-
-/*mongoConnect(()=>{
-  //console.log(client);
-  app.listen(3000);
-});*/
--- routes/tickets.js
+++ routes/tickets.js
@@ -19,14 +19,4 @@ router.get('/errorapi', errorController.getErrorAPI);
 
 router.get('*', errorController.get404);
 
-/*router.get('/cart', shopController.getCart);
-
-router.post('/cart', shopController.postCart);
-
-router.post('/cart-delete-item', shopController.postCartDeleteProduct);
-
-router.post('/create-order', shopController.postOrder);
-
-router.get('/orders', shopController.getOrders);*/
-
 module.exports = router;
